ef0fbdad4f4f4c30fac053797aebcfa648ffa79b
胡小安 <hu7006@outlook.com>
2023-02-25 17:47:20 +0800

    添加FPGA端工程

---
+++ FPGA_ioDetector/ioDetector.srcs/sources_1/new/main.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/02/25 17:10:24
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main(
+    input clk,
+    output [49:0] io
+    );
+    //
+
+
+endmodule
+
